32a1bd7f00c9f396fdd9279c11528593f5510167
Baldur Már Pétursson <baldurp12@ru.is>
2018-03-24 01:22:46 +0000

    Changed curl to wget

--- init-sh.sh
+++ init-sh.sh
@@ -1,11 +1,14 @@
 #!bin/bash
 
+
+cd /usr/src
+
 sudo apt update
 sudo apt-get update
 
 ## .NET CORE 2.1.4
 
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+wget https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
@@ -20,7 +23,7 @@ sudo apt-get install default-jdk
 
 ## GO LANG 1.10
 
-sudo curl https://dl.google.com/go/go1.10.linux-amd64.tar.gz
+sudo wget https://dl.google.com/go/go1.10.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.10.linux-amd64.tar.gz
 
 sudo export PATH=$PATH:/usr/local/go/bin
@@ -32,7 +35,6 @@ sudo apt-get install libreadline-gplv2-dev  libncursesw5-dev libssl-dev libsqlit
 
 sudo apt-get update
 
-cd /usr/src
 sudo wget https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tgz
 
 cd Python-3.6.4
@@ -40,7 +42,7 @@ sudo ./configure --enable-optimizations
 sudo make altinstall
 
 
-## Node.js - Including npm
+## Node.js 8.10.0 - Including npm
 
 curl --silent https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
 
@@ -53,12 +55,23 @@ echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a /e
 sudo apt-get update
 sudo apt-get install nodejs
 
+## Docker - THIS WILL GET THE LATEST VERSION OF DOCKER, NOT SUITABLE FOR PRODUCTION
 
-## React
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+wget -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-## Docker
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce
 
 ## SQLite
+
+sudo wget https://sqlite.org/2018/sqlite-autoconf-3220000.tar.gz
+sudo tar -xzf sqlite-autoconf-3220000.tar.gz
+
+sudo CFLAGS="-Os" ./configure; make;
+
 
 ## VISUAL STUDIO CODE
 
